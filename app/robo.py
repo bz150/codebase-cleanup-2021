de5260de0171bfca9be92bb144b27147fb70dc75
bz150 <77802960+bz150@users.noreply.github.com>
2021-04-04 22:59:23 -0400

    Robo exploration (#4)
    * Added get_response function
    
    pytest not working yet but robo advisor program works properly
    
    * pytest fixed

--- app/robo.py
+++ app/robo.py
@@ -7,45 +7,55 @@ from pandas import DataFrame
 import plotly.express as px
 
 load_dotenv()
+API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="abc123")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
-# FETCH DATA
+def get_response(symbol):
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(url) # issues an HTTP request
+    return json.loads(response.text)
 
-symbol = input("Please input a stock symbol (e.g. 'MSFT'): ")
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
-response = requests.get(request_url)
-parsed_response = json.loads(response.text)
+if __name__ == "__main__":
 
-# PROCESS DATA
+    # FETCH DATA
 
-records = []
-for date, daily_data in parsed_response["Time Series (Daily)"].items():
-    record = {
-        "date": date,
-        "open": float(daily_data["1. open"]),
-        "high": float(daily_data["2. high"]),
-        "low": float(daily_data["3. low"]),
-        "close": float(daily_data["4. close"]),
-        "volume": int(daily_data["5. volume"]),
-    }
-    records.append(record)
-
-df = DataFrame(records)
-
-# DISPLAY RESULTS
-
-print("LATEST CLOSING PRICE: ", records[0]["close"])
-print("LATEST CLOSING PRICE: ", df.iloc[0]["close"])
-print("RECENT HIGH: ", df["high"].max())
-print("RECENT LOW: ", df["low"].min())
-
-# EXPORT PRICES TO CSV
-
-csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", f"{symbol.lower()}_prices.csv")
-df.to_csv(csv_filepath)
-
-# CHART PRICES OVER TIME
-
-fig = px.line(df, y="close", title=f"Closing Prices for {symbol.upper()}") # see: https://plotly.com/python-api-reference/generated/plotly.express.line
-fig.show()
+    symbol = input("Please input a stock symbol (e.g. 'MSFT'): ")
+    parsed_response = get_response(symbol)
+    #request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    #response = requests.get(request_url)
+    #parsed_response = json.loads(response.text)
+    
+    # PROCESS DATA
+    
+    records = []
+    for date, daily_data in parsed_response["Time Series (Daily)"].items():
+        record = {
+            "date": date,
+            "open": float(daily_data["1. open"]),
+            "high": float(daily_data["2. high"]),
+            "low": float(daily_data["3. low"]),
+            "close": float(daily_data["4. close"]),
+            "volume": int(daily_data["5. volume"]),
+        }
+        records.append(record)
+    
+    df = DataFrame(records)
+    
+    # DISPLAY RESULTS
+    
+    print("LATEST CLOSING PRICE: ", records[0]["close"])
+    print("LATEST CLOSING PRICE: ", df.iloc[0]["close"])
+    print("RECENT HIGH: ", df["high"].max())
+    print("RECENT LOW: ", df["low"].min())
+    
+    # EXPORT PRICES TO CSV
+    
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", f"{symbol.lower()}_prices.csv")
+    df.to_csv(csv_filepath)
+    
+    # CHART PRICES OVER TIME
+    
+    fig = px.line(df, y="close", title=f"Closing Prices for {symbol.upper()}") # see: https://plotly.com/python-api-reference/generated/plotly.express.line
+    fig.show()
+    
