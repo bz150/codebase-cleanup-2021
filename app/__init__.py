b748790f9f276b0a4a2c6c0090fb6f7e817dcd70
Bryan Zhou <77802960+bz150@users.noreply.github.com>
2021-04-04 23:27:42 -0400

    to_usd placed in __init__
    import into shopping and robo

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,13 @@
 # ... facilitates local imports from one file to another,
 # ... including importing app code for testing purposes
 #
+
+def to_usd(my_price):
+    """
+    Formats a number as currency (USD) with dollar sign, two decimals, and thousands separator
+    
+    Params: my_price (numeric, int or float) to be formatted
+    
+    Examples: to_usd(21.645)
+    """
+    return f"${my_price:,.2f}"
